2a9262cfb1349bb9a03a5054faec57a46b885dce
Nguyen Le <lenguyenhcm325@gmail.com>
2023-03-22 23:25:55 +0100

    lesson 214 prepare to do some practice

--- src/store/categories/category.selector.ts
+++ src/store/categories/category.selector.ts
@@ -1,6 +1,8 @@
 import { createSelector } from "reselect";
 
-const selectCategoryReducer = (state) => {
+import { CategoriesState } from "./category.reducer";
+import { CategoryMap } from "./category.types";
+const selectCategoryReducer = (state): CategoriesState => {
   return state.categories;
 };
 
@@ -11,12 +13,12 @@ export const selectCategories = createSelector(
 
 export const selectCategoriesMap = createSelector(
   [selectCategories],
-  (categories) => {
+  (categories): CategoryMap => {
     return categories.reduce((acc, category) => {
       const { title, items } = category;
       acc[title.toLowerCase()] = items;
       return acc;
-    }, {});
+    }, {} as CategoryMap);
   }
 );
 
